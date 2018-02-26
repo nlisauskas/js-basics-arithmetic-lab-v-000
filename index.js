3781f22e12d785b7c74e875210f7b3e61936def3
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-02-26 12:54:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,3 +12,5 @@ const newID = oldID + 1000000000;
 const ageIsValid = Number.isInteger(currentAge);
 
 const randomNumber = Math.floor(Math.random() * 20) + 1;
+
+const randomInteger = Math.floor(randomNumber);
